06eac417f24bfa5a3b4d9f54267f178aa107a1cb
maribelcuales <maribelcuales@gmail.com>
2020-07-08 23:42:48 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AnimalForm from './components/AnimalForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Add New Animal
+      <AnimalForm />
+    </div>
+  );
+}
+
+export default App;
